6ae2dfe1b536f808c7c4edd5565d879e6fb3d8c8
Bibek Dallakoti <140939649+BibekDaBee@users.noreply.github.com>
2024-11-01 22:15:25 +1100

    fixing few bugs

--- lib/widgets/dashboard_widget.dart
+++ lib/widgets/dashboard_widget.dart
@@ -1,24 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:health/widgets/activity_details_card.dart';
 import 'package:health/widgets/header_widget.dart';
-import 'package:health/widgets/line_chart_card.dart';
 class DashboardWidget extends StatelessWidget {
   const DashboardWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
+    return const SingleChildScrollView(
       child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 18),
+        padding: EdgeInsets.symmetric(horizontal: 18),
         child: Column(
           children: [
-            const SizedBox(height: 18),
-            const HeaderWidget(),
-            const SizedBox(height: 18),
-            const ActivityDetailsCard(),
-            const SizedBox(height: 18),
-            const LineChartCard(),
-            const SizedBox(height: 18),
+            SizedBox(height: 18),
+            HeaderWidget(),
+            SizedBox(height: 18),
+            ActivityDetailsCard(),
+            SizedBox(height: 18),
           ],
         ),
       ),
